8c09d363fd0cf5cfab8bd6ab54a5a4c3f947fb22
Mike Timashov <mike@cherrypiestudio.com>
2024-04-08 11:47:21 +0200

    Share fix

--- src/composables/share.ts
+++ src/composables/share.ts
@@ -2,6 +2,6 @@ export const useShare = (func: Function, url: string, title: string) => {
   let res = func();
 
   // TODO transform html string to usual string
-  if (res == 'copy') navigator.clipboard.writeText(`https://claim.fund/public/${1}`);
+  if (res == 'copy') navigator.clipboard.writeText(`https://claim.fund/public/${url}`);
   else open(func(`https://claim.fund/public/${url}`, title));
 }
